adda3886fc0f2bdd51d56c7b7ab08fc8ed9a1c1d
Hadas Schlessinger <hadass@rezilion.com>
2020-06-17 02:19:33 +0300

    final

--- app/backend/calculatioins.py
+++ app/backend/calculatioins.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 
 def generate_airports(calc):
     if calc:
-        airport = pd.read_excel('app/flights_data.xlsx', 'airports')
+        airport = pd.read_excel('./flights_data.xlsx', 'airports')
         color_set = ["#5DADE2", "#F1C40F", "#E74C3C", "#3D3228", "#ECF0F1", "#73FF5B"]
         zones = airport['General_Time_Zone']
         unique_zones = zones.unique()
@@ -26,14 +26,14 @@ def generate_airports(calc):
 
 
 def generate_routes(airport):
-    routes = pd.read_excel('app/flights_data.xlsx', 'routes')
+    routes = pd.read_excel('./flights_data.xlsx', 'routes')
     routes = pd.DataFrame(routes)
     return list(_filter(airport, routes))
 
 
 def generate_popularity():
     print('executing')
-    routes = pd.read_excel('app/flights_data.xlsx', 'routes')
+    routes = pd.read_excel('./flights_data.xlsx', 'routes')
     routes = pd.DataFrame(routes)
     airports = routes['From'].unique()
 
@@ -48,12 +48,10 @@ def generate_popularity():
     for index in top_10_len:
         lengths.append(common_routes[index])
 
-    fig = go.Figure(
-        data=[go.Bar(x=airports, y=lengths)],
-        layout=dict(title=dict(text="Top 10 airports"))
-    )
+    plt.figure(figsize=(20,20))
 
-    fig.show()
+    plt.bar(airports, lengths)
+    plt.savefig('top_10.png')
 
 
 def _filter(airport, routes):
--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,8 @@ from .backend import calculatioins
 
 @app.route('/airports', methods=['POST', 'GET'])
 def airports():
-    calculatioins.generate_airports(False)
-    image = _encode_image('app/airports.png')
+    # calculatioins.generate_airports(False)
+    image = _encode_image('static/airports.png')
     return {'data': str(image)}
 
 
@@ -15,7 +15,7 @@ def airports():
 def routes():
     airport = request.form.get('airport')
     destiinations= calculatioins.generate_routes(airport)
-    image = _encode_image('app/routes.png')
+    image = _encode_image('static/routes.png')
     return {'data': str(image),
             'destinations': destiinations,
             'total': len(destiinations)}
@@ -23,8 +23,10 @@ def routes():
 @app.route('/popularity', methods=['POST'])
 def popularity():
     calculatioins.generate_popularity()
-    image = _encode_image('app/airports.png')
-    return {'data': str(image)}
+    image = _encode_image('static/airports.png')
+    image_p = _encode_image('./top_10.png')
+    return {'data': str(image),
+            'priority': str(image_p)}
 
 
 def _encode_image(image):
